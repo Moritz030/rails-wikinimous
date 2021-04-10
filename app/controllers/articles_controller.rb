d186690f6da9630d56229bf7550a9e11c8a81d5b
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-04-10 16:39:16 +0200

    edit action and view iomplemented, update action implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show]
+  before_action :set_article, only: [:show, :edit, :update]
   def index
     @articles = Article.all
   end
@@ -11,9 +11,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
     @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit
+
+  end
+  
+  def update
+    @article.update(article_params)
     redirect_to article_path(@article)
   end
 
