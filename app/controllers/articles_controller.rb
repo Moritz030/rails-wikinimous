07e93bb6ddfa8ecae05420abcd7eadd7fcb4f5dc
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-04-10 16:56:04 +0200

    show view modified --> destroy link added, destroy action implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
@@ -21,6 +21,11 @@ class ArticlesController < ApplicationController
   def edit
 
   end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
   
   def update
     @article.update(article_params)
@@ -35,4 +40,5 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:content, :title)
   end
+
 end
