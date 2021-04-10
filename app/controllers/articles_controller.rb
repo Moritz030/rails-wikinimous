f8aabf0f6f4056d056fcfa17b20354ca3b548b92
Moritz Froehlich <moritzfroehlich@gmail.com>
2021-04-10 16:14:18 +0200

    new action and view implementend & create action implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,24 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    
+
+  end
+
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 
   private
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:content, :title)
   end
 end
